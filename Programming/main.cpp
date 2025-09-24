a897562b66394f1a307165cb06662d3f36a3b1ba
Menuka <menuxff80@gmail.com>
2025-09-24 20:59:49 +0100

    Made a triangle using openGL

--- Programming/main.cpp
+++ Programming/main.cpp
@@ -1,56 +1,124 @@
-#include <iostream> 
+#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <cmath> // for sqrt()
+
+// Vertex shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+// Fragment shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"    FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\0";
 
 int main()
 {
-	// This is what initializes GLFW 
-	glfwInit();
+    // Initialize GLFW
+    glfwInit();
 
-	// This is what tells GLFW the version of OpenGL we are using
-	// And in this case we are using OpenGL 3.4 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); 
-	// This tells GLFW we are using the CORE profile (if your remember the glad version we installed thats what it is) 
-	// So that means we only have the modern functions 
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Set OpenGL version to 3.3 core profile
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// This creates a GLFWwindow of 800 by 800 pixels and it has been named "Gothic".
-	GLFWwindow* window = glfwCreateWindow(800, 800, "Gothic", NULL, NULL); 
-	// This error check if window fails to create 
-	if (window == NULL)
-	{
-		std::cout << "Failed to create the GLFW window" << std::endl; 
-		glfwTerminate(); 
-		return -1; 
-	}
-	// Introduce the window to the current context 
-	glfwMakeContextCurrent(window);
+    // Create a window
+    GLFWwindow* window = glfwCreateWindow(800, 800, "Gothic", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create the GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
 
-	// Load GLAD so it configures OpenGL 
-	gladLoadGL(); 
+    // Load GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
 
-	// Specify the viewport of OpenGl in the Window 
-	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-	glViewport(0, 0, 800, 800); 
+    // Viewport
+    glViewport(0, 0, 800, 800);
 
-	// Specify the color of the background 
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f); 
-	// Clean the back buffer and assign the new color to it 
-	glClear(GL_COLOR_BUFFER_BIT);
-	// Swapping the buffer with the front buffer 
-	glfwSwapBuffers(window); 
+    // Vertices for a triangle
+    GLfloat vertices[] =
+    {
+        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,   // Lower left
+         0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,   // Lower right
+         0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Top
+    };
 
-	// Main game loop 
-	while (!glfwWindowShouldClose(window))
-	{
-		// Take cares of all the GLFW events 
-		glfwPollEvents(); 
-	}
-	// Delete the window before ending the program 
-	glfwDestroyWindow(window); 
-	// Terminate GLFW before ending the program 
-	glfwTerminate(); 
-	return 0; 
+    // Build and compile shaders
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
 
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Delete shaders (already linked)
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Generate VAO & VBO
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    // Bind VAO
+    glBindVertexArray(VAO);
+
+    // Bind VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Vertex attribute pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Clear screen
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Cleanup
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
 }
